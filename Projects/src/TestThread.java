e494055938292738634b147b5dd38b441b980e3f
iluxonchik <iluxon4ik@hotmail.com>
2014-09-12 01:16:56 +0100

    More multithreading examples added.

--- Projects/src/TestThread.java
+++ Projects/src/TestThread.java
@@ -7,5 +7,11 @@ public class TestThread {
 		
 		R1.start();
 		R2.start();
+		
+		ThreadDemo T1 = new ThreadDemo("Thread-1");
+		ThreadDemo T2 = new ThreadDemo ("Thread-2");
+		
+		T1.start();
+		T2.start();
 	}
 }
